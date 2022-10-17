4045c7e41bf559f4b545e185adcd4d4445fb2fa4
Rakesh Anjani <rkanjani14@gmail.com>
2022-10-17 12:59:14 +0530

    question2.java

--- question2.java
+++ question2.java
@@ -1,100 +1,59 @@
-class question2
-{
-    private static Integer i1 = 0;
-    private static Integer i2 = 0;
-    private static Object lock1 = new Object();
-    private static Object lock2 = new Object();
+import java.util.ArrayList;
 
-    private static void increment() throws InterruptedException 
-    {
-        System.out.println(Thread.currentThread().getName() + " ++");
-        Thread.sleep(1000);
-        synchronized (lock1) 
-        {
-            i1++;
-        }
-        System.out.println(Thread.currentThread().getName() + " ++");
+import java.util.List;
+
+import java.util.concurrent.*;
+
+public class question2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Using new cached thread pool
+
+        ExecutorService ex = Executors.newCachedThreadPool();
+
+        List<Callable<Integer>> callables = new ArrayList<>();
+
+        Callable<Integer> callable = () -> {
+
+            Thread.sleep(300);
+
+            return 5;
+
+        };
+
+        callables.add(callable);
+
+        callables.add(callable);
+
+        callables.add(callable);
+
+        callables.add(callable);
+
+        callables.add(callable);
+
+        callables.add(callable);
+
+        List<Future<Integer>> futures = ex.invokeAll(callables);
+
+        System.out.println(Thread.activeCount());
+
+        futures.forEach(res -> {
+
+            try {
+
+                System.out.println("Result :: " + res.get());
+
+            } catch (InterruptedException | ExecutionException e) {
+
+                throw new RuntimeException(e);
+
+            }
+
+        });
+
+        ex.shutdown();
+
     }
 
-    private static void decrement() throws InterruptedException 
-    {
-        System.out.println(Thread.currentThread().getName() + " --");
-        Thread.sleep(1000);
-        synchronized (lock2) 
-        {
-            i2--;
-        }
-        System.out.println(Thread.currentThread().getName() + " --");
-    }
-
-    public static void main(String[] args) throws InterruptedException 
-    {
-        Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 20000; i++) 
-            {
-                try 
-                {
-                    increment();
-                } 
-                catch (InterruptedException e) 
-                {
-                    e.printStackTrace();
-                }
-            }
-        }, "Increment1");
-        
-        Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 20000; i++) 
-            {
-                try 
-                {
-                    decrement();
-                } 
-                catch (InterruptedException e) 
-                {
-                    e.printStackTrace();
-                }
-            }
-        }, "Decrement1");
-        
-        Thread t3 = new Thread(() -> {
-            for (int i = 0; i < 20000; i++) 
-            {
-                try 
-                {
-                    decrement();
-                } 
-                catch (InterruptedException e) 
-                {
-                    e.printStackTrace();
-                }
-            }
-        }, "Decrement2");
-        
-        Thread t4 = new Thread(() -> {
-            for (int i = 0; i < 20000; i++) 
-            {
-                try 
-                {
-                    increment();
-                } 
-                catch (InterruptedException e) 
-                {
-                    e.printStackTrace();
-                }
-            }
-        }, "Increment2");
-        
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t1.join();
-        t2.join();
-        t3.join();
-        t4.join();
-        
-        System.out.println("Value of Integer: " + i1);
-        System.out.println("Value of Integer: " + i2);
-    }
 }
